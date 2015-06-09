1fa86c71d338b1d8460f570bb0ccb001fad9d9d9
Lv_Sage <lv_sage@126.com>
2015-06-10 02:07:02 +0800

    5.8.2

--- myFlaskWeb.py
+++ myFlaskWeb.py
@@ -1,25 +1,50 @@
 # -*- coding: utf-8 -*-
-
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manage = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
+db = SQLAlchemy(app)
 
 class NameForm(Form):
     name = StringField(u'你的名字是？', validators=[DataRequired()])
     submit = SubmitField(u'提交')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.errorhandler(404)
 def page_not_found(e):
